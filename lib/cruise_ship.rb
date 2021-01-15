ceccba9b5165033818e51ab5c4855356a1a0fa80
Nate <7nater.nelson@gmail.com>
2021-01-14 20:45:09 -0700

    commit tree 1589242a31480b0e9ddd1b31404039483ea1f325

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,5 +7,11 @@
 # }
 
 def select_winner(passengers)
-  # add the code snippet here!
+  winner = ""
+  passengers.each do |key,value|
+    if key == :suite_a && value.downcase.start_with?("a")
+      winner = value
+    end
+  end
+  winner
 end
